498f5deb1cb4ae099a9f429ea6f7797160227353
Rodrigo Pietro Bacelli <rodrigobac@gmail.com>
2025-03-30 16:11:45 -0300

    feat: add basic home page structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App.tsx';
+import { Providers } from './providers';
+
+import './styles/global.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Providers>
+      <App />
+    </Providers>
+  </StrictMode>
+);
