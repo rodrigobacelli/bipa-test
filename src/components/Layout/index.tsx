92b135bb0d8e7df500db1e52ebcf2bd5730d9317
Rodrigo Pietro Bacelli <rodrigobac@gmail.com>
2025-03-30 21:46:28 -0300

    feat: add header to application

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,15 @@
+import { Header } from '../Header';
+import { Container } from '../Container';
+
 export type LayoutProps = {
   children?: React.ReactNode | React.ReactNode[];
 };
 
 export const Layout = ({ children }: LayoutProps) => {
-  return <div>{children}</div>;
+  return (
+    <div className="flex w-full flex-col items-center">
+      <Header />
+      <Container className="py-4">{children}</Container>
+    </div>
+  );
 };
